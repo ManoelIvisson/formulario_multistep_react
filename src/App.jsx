d4324d7dcc767b0725e866e64ee43babc1ae0cf5
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-02-05 17:55:48 -0300

    Criado os estilos das etapas

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {FiSend} from 'react-icons/fi'
 import UserForm from './components/UserForm'
 import ReviewForm from './components/ReviewForm'
 import Thanks from './components/UserForm copy'
+import Steps from './components/Steps'
 
 import './App.css'
 import { useForm } from './hooks/useForm'
@@ -18,33 +19,33 @@ function App() {
         <div className="header">
           <h2>Deixe sua avaliação</h2>
           <p>Ficamos felizes com a sua compra, utilize o formulário abaxio para avaliar o produto</p>
-          <div className='form-container'>
-            <p>etapas</p>
-            <form onSubmit={(event) => changeStep(currentStep + 1, event)}>
-              <div className="inputs-container">
-                {currentComponent}
-              </div>
-              <div className="actions">
-                {!isFirstStep && (
-                  <button type="button" onClick={() => changeStep(currentStep - 1)}>
-                    <GrFormPrevious />
-                    <span>Voltar</span>
-                  </button>
-                )}
-                {!isLastStep ? (
-                  <button type="submit">
-                    <span>Avançar</span>
-                    <GrFormNext />
-                  </button>
-                ) : (
-                  <button type="submit">
-                    <span>Enviar</span>
-                    <FiSend />
-                  </button> 
-                )}
-              </div>
-            </form>
-          </div>
+      </div>
+        <div className='form-container'>
+          <Steps currentStep={currentStep}/>
+          <form onSubmit={(event) => changeStep(currentStep + 1, event)}>
+            <div className="inputs-container">
+              {currentComponent}
+            </div>
+            <div className="actions">
+              {!isFirstStep && (
+                <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                  <GrFormPrevious />
+                  <span>Voltar</span>
+                </button>
+              )}
+              {!isLastStep ? (
+                <button type="submit">
+                  <span>Avançar</span>
+                  <GrFormNext />
+                </button>
+              ) : (
+                <button type="submit">
+                  <span>Enviar</span>
+                  <FiSend />
+                </button> 
+              )}
+            </div>
+          </form>
         </div>
       </div>
   )
