02950e5cfea1e847276b678dcc6e693f5aa33f54
Manoel Ívisson <manoelivisson4@hotmail.com>
2024-02-05 16:49:11 -0300

    Criado hook de movimentação nas etapas do formulário

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {GrFormPrevious, GrFormNext} from 'react-icons/gr'
+import {FiSend} from 'react-icons/fi'
+import UserForm from './components/UserForm'
+import ReviewForm from './components/ReviewForm'
+import Thanks from './components/UserForm copy'
+
 import './App.css'
+import { useForm } from './hooks/useForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const formComponents = [<UserForm />, <ReviewForm />, <Thanks />]
+
+  const {currentStep, currentComponent, changeStep, isLastStep, isFirstStep} = useForm(formComponents)
 
   return (
-      <div className='App'>
-        Formulario Multistep
+      <div className='app'>
+        <div className="header">
+          <h2>Deixe sua avaliação</h2>
+          <p>Ficamos felizes com a sua compra, utilize o formulário abaxio para avaliar o produto</p>
+          <div className='form-container'>
+            <p>etapas</p>
+            <form onSubmit={(event) => changeStep(currentStep + 1, event)}>
+              <div className="inputs-container">
+                {currentComponent}
+              </div>
+              <div className="actions">
+                {!isFirstStep && (
+                  <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                    <GrFormPrevious />
+                    <span>Voltar</span>
+                  </button>
+                )}
+                {!isLastStep ? (
+                  <button type="submit">
+                    <span>Avançar</span>
+                    <GrFormNext />
+                  </button>
+                ) : (
+                  <button type="submit">
+                    <span>Enviar</span>
+                    <FiSend />
+                  </button> 
+                )}
+              </div>
+            </form>
+          </div>
+        </div>
       </div>
   )
 }
---
+++ src/components/UserForm.jsx
@@ -0,0 +1,9 @@
+function UserForm() {
+    return (
+        <div>
+            <h2>User Form</h2>
+        </div>
+    )
+}
+
+export default UserForm
